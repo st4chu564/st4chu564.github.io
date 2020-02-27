524cb70b38ad98a1776469fbd2975dfabcdfe3e1
st4chu564 <stasiek94@gmail.com>
2020-02-27 18:03:19 +0100

    Further modifications, added some modals.

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    $("div.painting img.painting").click(function(e, target) {
+        let modalImg = $("div.modal-content img.painting-full");
+        let modalTitle = $("div.modal-content b.title")
+        let modalName = $("div.modal-content span.name");
+        let modalPlace = $("div.modal-content p.place");
+        console.log($(this).data("title"));
+        modalImg.attr("src", $(this).attr("src"));
+        modalTitle.html($(this).data("title"));
+        modalName.html($(this).data("name"));
+        modalPlace.html($(this).data("place"));
+        $("#painting-zoomed").modal('toggle');
+        $("body").css("padding-right", "0");
+    })
+})
