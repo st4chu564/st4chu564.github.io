2bf7ba1c1b56480c465c5d4a81324b50943e9f7f
st4chu564 <stasiek94@gmail.com>
2021-01-17 01:25:55 +0100

    Added multi language painting site

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,22 @@ $(function() {
         let modalName = $("div.modal-content span.name");
         let modalPlace = $("div.modal-content p.place");
         modalImg.attr("src", $(this).attr("src"));
-        modalTitle.html($(this).data("title"));
-        modalName.html($(this).data("name"));
-        modalPlace.html($(this).data("place"));
+        let language = sessionStorage.getItem("language");
+        if(language === null || language === 'polish'){
+            modalTitle.html($(this).data("title-polish"));
+            modalName.html($(this).data("name-polish"));
+            modalPlace.html($(this).data("place-polish"));
+        }
+        else if(language === 'english'){
+            modalTitle.html($(this).data("title-english"));
+            modalName.html($(this).data("name-english"));
+            modalPlace.html($(this).data("place-english"));
+        }
+        else if(language === 'french'){
+            modalTitle.html($(this).data("title-french"));
+            modalName.html($(this).data("name-french"));
+            modalPlace.html($(this).data("place-french"));
+        }
         $("#painting-zoomed").modal('toggle');
         $("#painting-zoomed .modal-dialog").css("max-width", "100%").width("auto").height("auto");
         $("body").css("padding-right", "0");
